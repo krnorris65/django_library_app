555261606e903c557fce0a2dcfc0959e15ee1217
Kristen Norris <krnorris65@gmail.com>
2020-05-04 10:11:24 -0500

    first five chapters of django library app

---
+++ libraryproject/libraryapp/views/libraries/list.py
@@ -0,0 +1,48 @@
+import sqlite3
+from django.shortcuts import render, redirect, reverse
+from libraryapp.models import Library
+from libraryapp.models import model_factory
+from ..connection import Connection
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def library_list(request):
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = model_factory(Library)
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            SELECT
+                l.id,
+                l.title,
+                l.address
+            FROM libraryapp_library l
+            """)
+
+            all_libraries = db_cursor.fetchall()
+
+        template_name = 'libraries/list.html'
+
+        context = {
+            'all_libraries': all_libraries
+        }
+
+        return render(request, template_name, context)
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO libraryapp_library
+            (
+                title, address
+            )
+            VALUES (?, ?)
+            """,
+            (form_data['title'], form_data['address']))
+
+        return redirect(reverse('libraryapp:libraries'))
