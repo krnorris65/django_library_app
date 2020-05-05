0163af0b7ae4e9e56d44d523f30d0a5197cc8546
Kristen Norris <krnorris65@gmail.com>
2020-05-05 11:47:29 -0500

    updated book details to see library and librarian

--- libraryproject/libraryapp/views/books/details.py
+++ libraryproject/libraryapp/views/books/details.py
@@ -6,44 +6,55 @@ from libraryapp.models import Book, Library, Librarian
 from libraryapp.models import model_factory
 from ..connection import Connection
 
+def create_book(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    book = Book()
+    book.id = _row["book_id"]
+    book.author = _row["author"]
+    book.isbn = _row["isbn"]
+    book.title = _row["title"]
+    book.year_published = _row["year_published"]
+
+    librarian = Librarian()
+    librarian.id = _row["librarian_id"]
+    librarian.first_name = _row["first_name"]
+    librarian.last_name = _row["last_name"]
+
+    library = Library()
+    library.id = _row["library_id"]
+    library.title = _row["library_name"]
+
+    book.librarian = librarian
+    book.location = library
+
+    return book
 
 def get_book(book_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Book)
+        conn.row_factory = create_book
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
         SELECT
-            b.id,
+            b.id book_id,
             b.title,
             b.isbn,
             b.author,
             b.year_published,
             b.librarian_id,
-            b.location_id
-        FROM libraryapp_book b
-        WHERE b.id = ?
-        """, (book_id,))
-
-        return db_cursor.fetchone()
-
-def get_librarian(librarian_id):
-    with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Librarian)
-        db_cursor = conn.cursor()
-
-        db_cursor.execute("""
-        SELECT
-            l.id,
-            l.location_id,
-            l.user_id,
+            b.location_id,
+            li.id librarian_id,
             u.first_name,
             u.last_name,
-            u.email
-        FROM libraryapp_librarian l
-        JOIN auth_user u ON l.user_id = u.id
-        WHERE l.id = ?
-        """, (librarian_id,))
+            loc.id library_id,
+            loc.title library_name
+        FROM libraryapp_book b
+        JOIN libraryapp_librarian li ON b.librarian_id = li.id
+        JOIN libraryapp_library loc ON b.location_id = loc.id
+        JOIN auth_user u ON u.id = li.user_id
+        WHERE b.id = ?
+        """, (book_id,))
 
         return db_cursor.fetchone()
 
@@ -51,12 +62,10 @@ def get_librarian(librarian_id):
 def book_details(request, book_id):
     if request.method == 'GET':
         book = get_book(book_id)
-        librarian = get_librarian(book.librarian_id)
 
         template = 'books/detail.html'
         context = {
-            'book': book,
-            'librarian': librarian
+            'book': book
         }
 
         return render(request, template, context)
