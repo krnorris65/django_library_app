1eb70230da5597dc48ed2de17a63ea58e83964f3
Kristen Norris <krnorris65@gmail.com>
2020-05-05 13:11:21 -0500

    user can see list of books for each library

--- libraryproject/libraryapp/views/books/details.py
+++ libraryproject/libraryapp/views/books/details.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from libraryapp.models import Book, Library, Librarian
-from libraryapp.models import model_factory
+# from libraryapp.models import model_factory
 from ..connection import Connection
 
 def create_book(cursor, row):
