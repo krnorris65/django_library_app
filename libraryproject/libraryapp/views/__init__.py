a4d021a7005526f76c1719ecf91131a9f63a4e3b
Kristen Norris <krnorris65@gmail.com>
2020-05-05 10:57:34 -0500

    user can edit a book

--- libraryproject/libraryapp/views/__init__.py
+++ libraryproject/libraryapp/views/__init__.py
@@ -1,10 +1,14 @@
-from .books.list import book_list
-from .librarians.list import librarian_list
-from .libraries.list import library_list
 from .home import home
 from .auth.logout import logout_user
+
+from .books.list import book_list
 from .books.form import book_form
-from .libraries.form import library_form
+from .books.form import book_edit_form
 from .books.details import book_details
-from .libraries.details import library_details
+
+from .librarians.list import librarian_list
 from .librarians.details import librarian_details
+
+from .libraries.list import library_list
+from .libraries.form import library_form
+from .libraries.details import library_details
