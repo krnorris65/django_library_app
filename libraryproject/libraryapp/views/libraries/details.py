762644a7b4e007f7b7d5dfd1b16e1182864add85
Kristen Norris <krnorris65@gmail.com>
2020-05-18 15:20:04 -0500

    changed libraries to orm

--- libraryproject/libraryapp/views/libraries/details.py
+++ libraryproject/libraryapp/views/libraries/details.py
@@ -7,26 +7,10 @@ from libraryapp.models import model_factory
 from ..connection import Connection
 
 
-def get_library(library_id):
-    with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Library)
-        db_cursor = conn.cursor()
-
-        db_cursor.execute("""
-        SELECT
-            l.id,
-            l.title,
-            l.address
-        FROM libraryapp_library l
-        WHERE l.id = ?
-        """, (library_id,))
-
-        return db_cursor.fetchone()
-
 @login_required
 def library_details(request, library_id):
     if request.method == 'GET':
-        library = get_library(library_id)
+        library = Library.objects.get(pk=library_id)
 
         template = 'libraries/detail.html'
         context = {
--- libraryproject/libraryapp/views/libraries/list.py
+++ libraryproject/libraryapp/views/libraries/list.py
@@ -5,90 +5,33 @@ from libraryapp.models import Library, Book
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
 
-def create_library(cursor, row):
-    _row = sqlite3.Row(cursor, row)
-
-    library = Library()
-    library.id = _row["id"]
-    library.title = _row["title"]
-    library.address = _row["address"]
-
-    # Note: You are adding a blank books list to the library object
-    # This list will be populated later (see below)
-    library.books = []
-
-    book = Book()
-    book.id = _row["book_id"]
-    book.title = _row["book_title"]
-    book.author = _row["author"]
-    book.isbn = _row["isbn"]
-    book.year_published = _row["year_published"]
-
-    # Return a tuple containing the library and the
-    # book built from the data in the current row of
-    # the data set
-    return (library, book,)
 
 @login_required
 def library_list(request):
     if request.method == 'GET':
-        with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = create_library
-            db_cursor = conn.cursor()
+        all_books = Book.objects.all()
+        all_libraries = Library.objects.all()
 
-            db_cursor.execute("""
-                SELECT
-                    li.id,
-                    li.title,
-                    li.address,
-                    b.id book_id,
-                    b.title book_title,
-                    b.author,
-                    b.year_published,
-                    b.isbn
-                FROM libraryapp_library li
-                JOIN libraryapp_book b ON li.id = b.location_id
-            """)
-
-            libraries = db_cursor.fetchall()
-            # Start with an empty dictionary
-            library_groups = {}
-
-            # Iterate the list of tuples
-            for (library, book) in libraries:
-
-                # If the dictionary does have a key of the current
-                # library's `id` value, add the key and set the value
-                # to the current library
-                if library.id not in library_groups:
-                    library_groups[library.id] = library
-                    library_groups[library.id].books.append(book)
-
-                # If the key does exist, just append the current
-                # book to the list of books for the current library
-                else:
-                    library_groups[library.id].books.append(book)
+        for library in all_libraries:
+            books = all_books.filter(location_id=library.id)
+            library.books = books
 
         template_name = 'libraries/list.html'
 
         context = {
-            'all_libraries': library_groups.values()
+            'all_libraries': all_libraries
         }
 
         return render(request, template_name, context)
     elif request.method == 'POST':
         form_data = request.POST
 
-        with sqlite3.connect(Connection.db_path) as conn:
-            db_cursor = conn.cursor()
+        new_library = Library(
+            title = form_data['title'], 
+            address = form_data['address']
+        )
 
-            db_cursor.execute("""
-            INSERT INTO libraryapp_library
-            (
-                title, address
-            )
-            VALUES (?, ?)
-            """,
-            (form_data['title'], form_data['address']))
+        new_library.save()
+                
 
         return redirect(reverse('libraryapp:libraries'))
